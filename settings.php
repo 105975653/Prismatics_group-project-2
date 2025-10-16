640e370bf2922fba9fa3ca2375f4fbdd729bf0ab
Farhan Monirul Islam <officialfarhan155@gmail.com>
2025-10-16 20:01:26 +1100

    Removed settings.php because i added only for checking, settings.php will be done by thisum

--- settings.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Database settings for XAMPP local server
-$host   = "localhost";  // usually localhost
-$user   = "root";       // default XAMPP MySQL username
-$pwd    = "";           // default XAMPP MySQL password is empty
-$sql_db = "jobs_db"; // your database name
-?>
